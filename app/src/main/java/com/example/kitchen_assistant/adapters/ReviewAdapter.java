ce09a8febc6d8362861e391efd58edf510c8a9d7
truonghh99 <truonghh@beloit.edu>
2020-07-22 23:20:57 -0700

    Add review adapter

---
+++ app/src/main/java/com/example/kitchen_assistant/adapters/ReviewAdapter.java
@@ -0,0 +1,89 @@
+package com.example.kitchen_assistant.adapters;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kitchen_assistant.R;
+import com.example.kitchen_assistant.activities.MainActivity;
+import com.example.kitchen_assistant.databinding.ItemRecipeBinding;
+import com.example.kitchen_assistant.databinding.ItemReviewBinding;
+import com.example.kitchen_assistant.fragments.recipes.CurrentRecipeDetailFragment;
+import com.example.kitchen_assistant.fragments.recipes.NewRecipeDetailFragment;
+import com.example.kitchen_assistant.helpers.GlideHelper;
+import com.example.kitchen_assistant.models.Product;
+import com.example.kitchen_assistant.models.Recipe;
+import com.example.kitchen_assistant.models.Review;
+import com.example.kitchen_assistant.storage.CurrentRecipes;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder> {
+
+    private final String TAG = "ReviewAdapter";
+    private Context context;
+    private List<Review> reviews;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemReviewBinding itemReviewBinding = ItemReviewBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(itemReviewBinding);
+    }
+
+    public ReviewAdapter(Context context, List<Review> reviews) {
+        this.context = context;
+        this.reviews = reviews;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Review review = reviews.get(position);
+        holder.bind(review);
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+
+    public void replaceAll(List<Review> filteredReviews) {
+        reviews = filteredReviews;
+        notifyDataSetChanged();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvReviewTitle;
+        private TextView tvReviewContent;
+        private ItemReviewBinding itemReviewBinding;
+        private ImageView ivProfileImage;
+        private RatingBar ratingBar;
+
+        public ViewHolder(@NonNull ItemReviewBinding itemReviewBinding) {
+            super(itemReviewBinding.getRoot());
+            this.itemReviewBinding = itemReviewBinding;
+            tvReviewTitle = itemReviewBinding.tvReviewTitle;
+            tvReviewContent = itemReviewBinding.tvReviewContent;
+            ivProfileImage = itemReviewBinding.ivProfileImage;
+            ratingBar = itemReviewBinding.ratingBar;
+        }
+
+        public void bind(final Review review) {
+        }
+    }
+
+}
